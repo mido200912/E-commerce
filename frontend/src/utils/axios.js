2c1c9c8dbb63d7cc6f9c9f4c4d00958830af075f
mido200912 <mohammedalsyedalsyed12@gmail.com>
2026-02-17 21:38:57 +0200

    Update axios config for proxy support

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const instance = axios.create({
-    baseURL: 'https://shop-rahhalah.vercel.app', // Hardcoded to ensure it works
+    baseURL: import.meta.env.PROD ? 'https://shop-rahhalah.vercel.app' : '', // Empty string for dev to use Proxy
     withCredentials: true
 });
 
