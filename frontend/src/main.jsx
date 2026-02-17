faf804bf63bf408f1887b4b60a4e19b1320573eb
mido200912 <mohammedalsyedalsyed12@gmail.com>
2026-02-17 18:34:21 +0200

    first commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import axios from 'axios'
 import './index.css'
+
+// Set backend URL
+axios.defaults.baseURL = 'https://shop-rahhalah.vercel.app';
+axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
